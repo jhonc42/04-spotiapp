2a3ed0aacea59b88026e7e62ae3f5bf1e3a7521b
jhonc42 <jhon_alex42@hotmail.com>
2019-10-30 17:59:59 -0500

    controlando errores del servicio api

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,12 +10,20 @@ import { SpotifyService } from '../../services/spotify.service';
 export class HomeComponent {
 nuevasCanciones: any[] = [];
 loading: boolean;
+error: boolean;
+mensajeError: string;
   constructor( private spotify: SpotifyService ) {
     this.loading = true;
+    this.error = false;
     this.spotify.getNewReleases()
     .subscribe( (data: any) => {
       this.nuevasCanciones = data;
       this.loading = false;
+    }, (errorServicio) => {
+      this.error = true;
+      this.loading = false;
+      console.log(errorServicio);
+      this.mensajeError = errorServicio.error.error.message;
     });
    }
 
